8fcdffae415e5cfbabc86eb1b7e00b36996fbb4d
Jesús Valeta <jvaleta1995@gmail.com>
2024-06-26 11:03:09 -0500

    Cofiguracion inicial del proyecto

---
+++ donatello/src/main/java/com/igrowker/donatello/DonatelloApplication.java
@@ -0,0 +1,13 @@
+package com.igrowker.donatello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatelloApplication.class, args);
+	}
+
+}
---
+++ donatello/src/test/java/com/igrowker/donatello/DonatelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igrowker.donatello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
