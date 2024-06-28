1c1b75e3d96eb280961fe651628a5f0167f0c0f2
unknown <erik.argel.ungs@gmail.com>
2024-06-28 11:45:56 -0300

    correccion de la URL del endpoint clientes

--- donatello/src/main/java/com/igrowker/donatello/controllers/ClienteController.java
+++ donatello/src/main/java/com/igrowker/donatello/controllers/ClienteController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("api/customers")
 public class ClienteController {
 
     @Autowired
