2aad562858eb58fbaae224eba97e8fbdc2b17c77
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-07-02 15:33:00 -0300

    fix (Exceptions) creacion de tipo bad credentials y aplicacion en authenticacion de usuarios

--- donatello/src/main/java/com/igrowker/donatello/exceptions/MANEJO_PENDIENTE_BORRAR/AlreadyExistException.java
+++
@@ -1,5 +0,0 @@
-package com.igrowker.donatello.exceptions.MANEJO_PENDIENTE_BORRAR;
-
-public class AlreadyExistException extends RuntimeException{
-    public AlreadyExistException(String msg){ super(msg) ; }
-}
