cb370d1adee044a0c6c21b910221dcdc10fdfa6e
Jesús Valeta <jvaleta1995@gmail.com>
2024-07-02 17:32:37 -0500

    Se creo el repository, service y controller para gestion de inventario

--- donatello/src/main/java/com/igrowker/donatello/repositories/ProductoRepository.java
+++ donatello/src/main/java/com/igrowker/donatello/repositories/ProductoRepository.java
@@ -1,2 +1,11 @@
-package com.igrowker.donatello.repositories;public interface ProductoRepository {
+package com.igrowker.donatello.repositories;
+
+import com.igrowker.donatello.models.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+    Boolean existsByNombre(String nombre);
 }
--- donatello/src/main/java/com/igrowker/donatello/services/ProductoService.java
+++ donatello/src/main/java/com/igrowker/donatello/services/ProductoService.java
@@ -1,2 +1,16 @@
-package com.igrowker.donatello.services;public interface ProductoService {
+package com.igrowker.donatello.services;
+
+import com.igrowker.donatello.dtos.ProductoDto;
+
+import java.util.List;
+
+public interface ProductoService {
+
+    ProductoDto save(ProductoDto productoDto);
+
+    List<ProductoDto> getProductos();
+
+    ProductoDto update(Integer productoId, ProductoDto productoDto);
+
+    ProductoDto delete(Integer productoId);
 }
