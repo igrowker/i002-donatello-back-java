dc2bb91da92f097a8c4306b9e6c336df9eadb640
Jesús Valeta <jvaleta1995@gmail.com>
2024-07-03 18:02:40 -0500

    Se creo el repository, service y controller para la gestion de promociones

--- donatello/src/main/java/com/igrowker/donatello/repositories/PromocionRepository.java
+++ donatello/src/main/java/com/igrowker/donatello/repositories/PromocionRepository.java
@@ -1,2 +1,9 @@
-package com.igrowker.donatello.repositories;public interface PromocionRepository {
+package com.igrowker.donatello.repositories;
+
+import com.igrowker.donatello.models.Promocion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromocionRepository extends JpaRepository<Promocion, Integer> {
 }
