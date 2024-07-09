3b7b69b1a607ea8f60aac869ba0dc8891d598627
Jesús Valeta <jvaleta1995@gmail.com>
2024-07-09 17:30:54 -0500

    se creo el metodo para listar menu por id usuario

--- donatello/src/main/java/com/igrowker/donatello/repositories/MenuRepository.java
+++ donatello/src/main/java/com/igrowker/donatello/repositories/MenuRepository.java
@@ -4,6 +4,9 @@ import com.igrowker.donatello.models.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+    List<Menu> findByIdUser(Integer userId);
 }
