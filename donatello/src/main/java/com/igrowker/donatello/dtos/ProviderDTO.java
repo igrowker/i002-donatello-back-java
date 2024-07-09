78f2dfb9b603a3e652b55e00b38059a2360426f5
Emanuel <ema_magallanes@hotmail.com>
2024-07-09 17:09:37 -0300

    Provider DTO added.

---
+++ donatello/src/main/java/com/igrowker/donatello/dtos/ProviderDTO.java
@@ -0,0 +1,14 @@
+package com.igrowker.donatello.dtos;
+
+import com.igrowker.donatello.auth.entities.CustomUser;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProviderDTO {
+    private String name;
+    private String contact;
+    private String address;
+    private CustomUser user;
+}
