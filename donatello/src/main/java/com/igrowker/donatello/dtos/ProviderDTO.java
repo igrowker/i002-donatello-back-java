46c36827f4512e0e8270429ccf695d9bd08b7b89
Emanuel <ema_magallanes@hotmail.com>
2024-07-12 20:50:07 -0300

    Adding constructors

--- donatello/src/main/java/com/igrowker/donatello/dtos/ProviderDTO.java
+++ donatello/src/main/java/com/igrowker/donatello/dtos/ProviderDTO.java
@@ -1,11 +1,14 @@
 package com.igrowker.donatello.dtos;
 
 import com.igrowker.donatello.auth.entities.CustomUser;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor @NoArgsConstructor
 public class ProviderDTO {
     private Long id;
     private String name;
